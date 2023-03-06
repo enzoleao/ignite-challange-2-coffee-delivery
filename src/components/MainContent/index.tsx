02c2e9ca026a0452873e28f68b8512b6b6a6e8cd
enzoleao <enzopinheiro6@gmail.com>
2023-03-06 16:10:26 -0300

    update

--- src/components/MainContent/index.tsx
+++ src/components/MainContent/index.tsx
@@ -4,10 +4,14 @@ export function MainContect() {
   return (
     <div className={styles.mainWrapper}>
       <div className={styles.mainContent}>
-        <section>Title</section>
-        <section>
-          <img src={coffeeImage} alt="coffee-image" />
+        <section className={styles.titlesSections}>
+          <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+          <p>
+            Com o Coffee Delivery você recebe seu café onde estiver, a qualquer
+            hora
+          </p>
         </section>
+        <img src={coffeeImage} alt="coffee-image" />
       </div>
     </div>
   )
