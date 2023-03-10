7c8d0f2a63a828fd3edee74b8eb08000b3b06001
Chrzddd <enzopinheiro6@gmail.com>
2023-03-09 22:19:24 -0300

    update

--- src/components/FineshCheckout/index.tsx
+++ src/components/FineshCheckout/index.tsx
@@ -1,5 +1,6 @@
 import styles from './FineshCheckout.module.scss'
 import deliveryImage from '../../assets/deliveryImage.png'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
 export function FineshedCheckout() {
   return (
     <div className={styles.fineshCheckoutWrapper}>
@@ -13,18 +14,22 @@ export function FineshedCheckout() {
               </p>
             </header>
             <div className={styles.informationsDeliveryBox}>
-              <div>
+              <div className={styles.boxInformations}>
+                <MapPin className={styles.checkoutIconLocation} />
                 <span>
                   <p>Entrega em</p>
                   <p>Farrapos - Porto Alegre</p>
                 </span>
               </div>
-              <div>
+              <div className={styles.boxInformations}>
+                <Timer className={styles.checkoutIconTimer} />
                 <span>
                   <p>Previsão de Entrega</p>
+                  <p>20 - 30 minutos</p>
                 </span>
               </div>
-              <div>
+              <div className={styles.boxInformations}>
+                <CurrencyDollar className={styles.checkoutIconCurrencyMoney} />
                 <span>
                   <p>Pagamento</p>
                 </span>
